8e762f233be141daf8f3a6d66596300f1e38ec1a
hprs0310 <128789017+hprs0310@users.noreply.github.com>
2023-11-14 18:51:31 +0530

    Add files via upload

---
+++ Task 3/react-profile-master-main/react-profile-master-main/README.md
@@ -0,0 +1,1 @@
+This the simnple profile application # react-profile-master
---
+++ Task 3/react-profile-master-main/react-profile-master-main/react-profile-master/src/assets/data/contact.js
@@ -0,0 +1,16 @@
+export const contact = {
+  name: "Truong Nguyen Quoc Dat",
+  job: "Web Developer",
+  avatar: {
+    src: "https://i.ibb.co/k2025PM/image.png",
+    alt: "quoc-dat-wears-a-mask"
+  },
+  phone: "0379557887",
+  email: "quocdat1994@gmail.com",
+  linkedin: "https://www.linkedin.com/in/dattruongnq/",
+  skype: "skype:live:quocdat1994_3?chat",
+  codepen: "https://codepen.io/quocdat1994/",
+  github: "https://github.com/QuocDat1994",
+  intro:
+    "Hello! I’m Dat! I love creation. I want to create something that everybody can enjoy. That's why I become a developer! Until now, I have been working for 3 years, specializing in Front - End development.I love Javascript, as well as its frameworks jQuery and React.js. My experiences have taught me that understanding 'the core' is essential in programming, no matter which languages you are using. I also enjoy researching new technology.Learning is a never- ending journey, gotta keep learning right?!"
+};
